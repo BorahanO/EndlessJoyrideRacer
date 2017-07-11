250b44e76740b1583bdc6431c5eb50891bb95ffc
mrSmellyfeet100 <boraoxal@gmail.com>
2017-07-11 09:58:27 +0200

    Added Jetpack

---
+++ Endless/Assets/Scripts/CarController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour {
+
+	public float jetpackForce = 75.0f;
+	public Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		
+		rb = GetComponent<Rigidbody2D>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate () 
+	{
+		bool jetpackActive = (Input.GetKey ("space"));
+
+		if (jetpackActive)
+		{
+			rb.AddForce(new Vector2(0, jetpackForce));
+		}
+	}
+}
