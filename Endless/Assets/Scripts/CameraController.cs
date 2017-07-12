79e01b179d1b7e48b6df95507a0c446c59040c04
mrSmellyfeet100 <boraoxal@gmail.com>
2017-07-12 11:40:39 +0200

    Camera follows player

---
+++ Endless/Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	//Allows to set a target for the camera to follow
+	public GameObject targetObject;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float targetObjectX = targetObject.transform.position.x;
+
+		Vector3 newCameraPosition = transform.position;
+		newCameraPosition.x = targetObjectX;
+		transform.position = newCameraPosition;
+		
+	}
+}
