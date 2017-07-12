3fc8599349644a3bf32be2a30f5fd27f989a98ea
mrSmellyfeet100 <boraoxal@gmail.com>
2017-07-12 11:31:42 +0200

    Added Automatic Car movement

--- Endless/Assets/Scripts/CarController.cs
+++ Endless/Assets/Scripts/CarController.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour {
 
+	// Variables public for easy In-Engine Edit
+	// Speed of car movement
+	public float Speed = 3.0f;
+	// Power of jetpack
 	public float jetpackForce = 75.0f;
+	// Rigidbody for gravity
 	public Rigidbody2D rb;
 
 	// Use this for initialization
 	void Start () {
 		
 		rb = GetComponent<Rigidbody2D>();
-
 	}
 	
 	// Update is called once per frame
@@ -21,11 +25,16 @@ public class CarController : MonoBehaviour {
 
 	void FixedUpdate () 
 	{
+		//Jetpack controls
 		bool jetpackActive = (Input.GetKey ("space"));
 
 		if (jetpackActive)
 		{
 			rb.AddForce(new Vector2(0, jetpackForce));
 		}
+		//Automatic movement
+		Vector2 newVelocity = GetComponent<Rigidbody2D>().velocity;
+		newVelocity.x = Speed;
+		GetComponent<Rigidbody2D>().velocity = newVelocity;
 	}
 }
