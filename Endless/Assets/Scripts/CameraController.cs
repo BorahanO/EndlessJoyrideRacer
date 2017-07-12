69293b4533264c0010418bc1763a2ac037f989bf
mrSmellyfeet100 <boraoxal@gmail.com>
2017-07-12 14:01:27 +0200

    Added Room Generator

--- Endless/Assets/Scripts/CameraController.cs
+++ Endless/Assets/Scripts/CameraController.cs
@@ -7,18 +7,21 @@ public class CameraController : MonoBehaviour {
 	//Allows to set a target for the camera to follow
 	public GameObject targetObject;
 
-	//Set the camera at a certain distance
+	//Set private variable for car distance to camera
 	private float distanceToTarget;
 
 	// Use this for initialization
 	void Start () {
-		
+
+		// calculate camera offset 
 		distanceToTarget = transform.position.x - targetObject.transform.position.x;
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		// Apply carsprite distance to camera offset
 		float targetObjectX = targetObject.transform.position.x;
 
 		Vector3 newCameraPosition = transform.position;
---
+++ Endless/Assets/Scripts/roomGenerator.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class roomGenerator : MonoBehaviour {
+
+	public GameObject[] availableRooms;
+
+	public List<GameObject> currentRooms;
+
+	private float screenWidthInPoints;
+
+	// Use this for initialization
+	void Start () {
+		// Calculate size of screen in points
+		float height = 2.0f * Camera.main.orthographicSize;
+		screenWidthInPoints = height * Camera.main.aspect;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void AddRoom(float farhtestRoomEndX)
+	{
+		// Pick random index of room to generate
+		int randomRoomIndex = Random.Range(0, availableRooms.Length);
+
+		// Creates room object from array of available rooms
+		GameObject room = (GameObject)Instantiate(availableRooms[randomRoomIndex]);
+
+		// Takes the size of the RoomFloor (width of room) object
+		float roomWidth = room.transform.FindChild("floor").localScale.x;
+
+		// Sets point where new room should be added
+		float roomCenter = farhtestRoomEndX + roomWidth * 0.5f;
+
+		// Sets the X-coordinate of new room
+		room.transform.position = new Vector3(roomCenter, 0, 0);
+
+		// Adds the room to current list of rooms
+		currentRooms.Add(room);         
+	} 
+}
